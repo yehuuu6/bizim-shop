34c60dbaee02dc37f915f748fb635a01660516d4
sscorpi <therenaydin@gmail.com>
2023-08-27 01:53:45 +0300

    Moved other core functions aswell.

--- assets/js/dashboard/utils/functions.js
+++ assets/js/dashboard/utils/functions.js
@@ -1,5 +1,6 @@
 import { ProfilePage } from "../user.js";
 
+// Directly gets response and displays the message. Use this if you don't need to do stuff with response.
 export function getApiResponse(panelObject, formData, scrollTo) {
     panelObject.sendApiRequest(formData).then((data) => {
         if (JSON.parse(data)[0] === "success" && panelObject == ProfilePage) clearAvatarInput();
@@ -46,4 +47,51 @@ export function getDate(raw) {
     const month = month_names[Number(month_t) - 1];
     const day_trimmed = Number(day).toString();
     return `${month} ${day_trimmed}, ${year}`;
+}
+
+export function getCategory(id) {
+    let category = "";
+    id = parseInt(id);
+    switch (id) {
+        case 1:
+        category = "Müzik Seti";
+        break;
+        case 2:
+        category = "Hoparlör";
+        break;
+        case 3:
+        category = "Plak Çalar";
+        break;
+        case 4:
+        category = "Müzik Çalar";
+        break;
+    }
+    return category;
+};
+  
+export function getPerm(id) {
+    let perm = "";
+    id = parseInt(id);
+    switch (id) {
+        case 0:
+        perm = "Üye";
+        break;
+        case 1:
+        perm = "Yönetici";
+        break;
+    }
+    return perm;
+};
+  
+export function setStatus(status) {
+    let statusText = "";
+    switch (status) {
+        case "1":
+        statusText = "Listeleniyor";
+        break;
+        case "0":
+        statusText = "Listelenmiyor";
+        break;
+    }
+    return statusText;
 }
