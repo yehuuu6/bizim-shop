7d7ce7117615309f12ce00ff7017ea8c24223a85
sscorpi <therenaydin@gmail.com>
2023-08-27 23:53:01 +0300

    Reformatted using Prettier extension.

--- dashboard.php
+++ dashboard.php
@@ -142,12 +142,12 @@ while ($row3 = mysqli_fetch_assoc($res)) {
       <section id="change-user-info" data-url="profile" data-title="Profili Düzenle" class="page-content narrow-page">
         <div id="loader-profile" class="loader">
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin:auto;background:transparent;display:block;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-<circle cx="50" cy="50" r="30" stroke="#a1a1a1" stroke-width="10" fill="none"></circle>
-<circle cx="50" cy="50" r="30" stroke="#6200ff" stroke-width="8" stroke-linecap="round" fill="none">
-    <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;180 50 50;720 50 50" keyTimes="0;0.5;1"></animateTransform>
-    <animate attributeName="stroke-dasharray" repeatCount="indefinite" dur="1s" values="18.84955592153876 169.64600329384882;94.2477796076938 94.24777960769377;18.84955592153876 169.64600329384882" keyTimes="0;0.5;1"></animate>
-</circle>
-</svg>
+            <circle cx="50" cy="50" r="30" stroke="#a1a1a1" stroke-width="10" fill="none"></circle>
+            <circle cx="50" cy="50" r="30" stroke="#6200ff" stroke-width="8" stroke-linecap="round" fill="none">
+              <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;180 50 50;720 50 50" keyTimes="0;0.5;1"></animateTransform>
+              <animate attributeName="stroke-dasharray" repeatCount="indefinite" dur="1s" values="18.84955592153876 169.64600329384882;94.2477796076938 94.24777960769377;18.84955592153876 169.64600329384882" keyTimes="0;0.5;1"></animate>
+            </circle>
+          </svg>
         </div>
         <div class="content-header">
           <div class="item">
@@ -233,8 +233,8 @@ while ($row3 = mysqli_fetch_assoc($res)) {
             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin:auto;background:transparent;display:block;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
               <circle cx="50" cy="50" r="30" stroke="#a1a1a1" stroke-width="10" fill="none"></circle>
               <circle cx="50" cy="50" r="30" stroke="#6200ff" stroke-width="8" stroke-linecap="round" fill="none">
-                  <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;180 50 50;720 50 50" keyTimes="0;0.5;1"></animateTransform>
-                  <animate attributeName="stroke-dasharray" repeatCount="indefinite" dur="1s" values="18.84955592153876 169.64600329384882;94.2477796076938 94.24777960769377;18.84955592153876 169.64600329384882" keyTimes="0;0.5;1"></animate>
+                <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;180 50 50;720 50 50" keyTimes="0;0.5;1"></animateTransform>
+                <animate attributeName="stroke-dasharray" repeatCount="indefinite" dur="1s" values="18.84955592153876 169.64600329384882;94.2477796076938 94.24777960769377;18.84955592153876 169.64600329384882" keyTimes="0;0.5;1"></animate>
               </circle>
             </svg>
           </div>
@@ -273,9 +273,9 @@ while ($row3 = mysqli_fetch_assoc($res)) {
               <tbody>
               </tbody>
             </table>
-              <button class="btn success-btn" id="load-more-products">
-                Daha fazla yükle
-              </button>
+            <button class="btn success-btn" id="load-more-products">
+              Daha fazla yükle
+            </button>
           </div>
         </section>
         <section id="add-product" data-url="add-product" data-title="Ürün Ekle" class="page-content narrow-page">
@@ -283,8 +283,8 @@ while ($row3 = mysqli_fetch_assoc($res)) {
             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin:auto;background:transparent;display:block;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
               <circle cx="50" cy="50" r="30" stroke="#a1a1a1" stroke-width="10" fill="none"></circle>
               <circle cx="50" cy="50" r="30" stroke="#6200ff" stroke-width="8" stroke-linecap="round" fill="none">
-                  <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;180 50 50;720 50 50" keyTimes="0;0.5;1"></animateTransform>
-                  <animate attributeName="stroke-dasharray" repeatCount="indefinite" dur="1s" values="18.84955592153876 169.64600329384882;94.2477796076938 94.24777960769377;18.84955592153876 169.64600329384882" keyTimes="0;0.5;1"></animate>
+                <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;180 50 50;720 50 50" keyTimes="0;0.5;1"></animateTransform>
+                <animate attributeName="stroke-dasharray" repeatCount="indefinite" dur="1s" values="18.84955592153876 169.64600329384882;94.2477796076938 94.24777960769377;18.84955592153876 169.64600329384882" keyTimes="0;0.5;1"></animate>
               </circle>
             </svg>
           </div>
