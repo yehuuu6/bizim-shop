22ed793013bca1fed4f675a151f42804ecf8b33c
sscorpi <therenaydin@gmail.com>
2023-08-29 15:21:55 +0300

    Moved some assets to the src file for webpack.

---
+++ src/interactive.js
@@ -0,0 +1,10 @@
+const wishlistBtn = document.querySelector("#wishlist-btn");
+const cartBtn = document.querySelector("#cart-btn");
+
+wishlistBtn.addEventListener("click", () => {
+  console.log("wishlist");
+});
+
+cartBtn.addEventListener("click", () => {
+  console.log("cart");
+});
