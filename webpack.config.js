e056f3b4468cd755c91c4b0a11d93f2889429d5b
sscorpi <therenaydin@gmail.com>
2023-09-02 23:56:09 +0300

    Included menu in the bundle

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ const path = require("path");
 
 module.exports = {
   watch: false,
-  mode: "production",
+  mode: "development",
   entry: {
     "a/a578a8g": path.resolve(__dirname, "src/auth/auth.ts"),
     "d/du48gn1": path.resolve(__dirname, "src/dashboard/user.ts"),
     "d/da48gn2": path.resolve(__dirname, "src/dashboard/dev.ts"),
+    "d/dm48gfz": path.resolve(__dirname, "src/dashboard/menu.ts"),
   },
   output: {
     path: path.resolve(__dirname, "dist"),
