8198e962cb722cae90413636e308309e90f3a520
sscorpi <therenaydin@gmail.com>
2023-09-02 19:21:17 +0300

    New API endpoint for profile page

---
+++ api/dashboard/users/get-profile.php
@@ -0,0 +1,21 @@
+<?php
+define('FILE_ACCESS', TRUE);
+if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')) {
+    require_once("{$_SERVER['DOCUMENT_ROOT']}/config/authenticator.php");
+
+    $id = $_SESSION['id'];
+
+    $sql = "SELECT profile_image, district, city FROM users WHERE id = ?";
+    $stmt = mysqli_prepare($con, $sql);
+    mysqli_stmt_bind_param($stmt, 'i', $id);
+    mysqli_stmt_execute($stmt);
+    $res = mysqli_stmt_get_result($stmt);
+
+    $row = mysqli_fetch_assoc($res);
+
+    echo json_encode($row);
+} else {
+    header("HTTP/1.1 403 Forbidden");
+    include($_SERVER['DOCUMENT_ROOT'] . '/errors/403.html');
+    exit;
+}
