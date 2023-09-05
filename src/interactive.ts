af4fbee276a645865141a39148e623495e047fe8
sscorpi <therenaydin@gmail.com>
2023-09-05 20:42:12 +0300

    Integrated to TypeScript

---
+++ src/interactive.ts
@@ -0,0 +1,13 @@
+import "./styles/main.css";
+import "./styles/utils.css";
+
+const wishlistBtn = document.querySelector("#wishlist-btn") as HTMLButtonElement;
+const cartBtn = document.querySelector("#cart-btn") as HTMLButtonElement;
+
+wishlistBtn.addEventListener("click", () => {
+  console.log("wishlist");
+});
+
+cartBtn.addEventListener("click", () => {
+  console.log("cart");
+});
