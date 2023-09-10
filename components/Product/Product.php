57a1df3b1281ac69c119be4ff768bf92cfdac807
sscorpi <therenaydin@gmail.com>
2023-09-10 19:58:16 +0300

    Changed product card component

--- components/Product/Product.php
+++ components/Product/Product.php
@@ -13,32 +13,30 @@ class Product extends Component
 {
     public function __construct(array $product)
     {
+        $product_title = parent::shorten_string($product['name'], 15);
         $body = <<<HTML
-        <div class="product">
-            <div class="product-image"> 
-                <img src="{$this->get_image_src($product)}" alt="Product Image">
-                <div class="product-info">
-                <div class="product-name">
-                    <h3>{$product["name"]}</h3>
-                </div>
-                <div class="product-price">
-                    <h3>₺{$product['price']}</h3>
-                </div>
-                <div class="product-description">
-                    <p>{$product['description']}</p>
-                </div>
-                <div class="product-buttons">
-                    <button class="add-to-cart">Add to Cart</button>
-                    <button class="view-product">View Details</button>
-                </div>
+        <div class="product" data-id="{$product['id']}">
+            <div class="product-image">
+                {$this->render_wishlist_element(false)}
+                <img src="{$this->get_image_src($product)}" alt="Resim">
+                {$this->render_shipment_element($product)}
             </div>
+            <div class="product-info">
+                <a title="{$product['name']}" href="#" class="product-title">{$product_title}</a>
+                <span class="product-price">₺{$product['price']}</span>
             </div>
+            <button class="add-cart">Sepete Ekle</button>
         </div>
         HTML;
 
         // Render the component on the page
-        $this->render($body);
+        parent::render($body);
     }
+    /**
+     * Gets the image source of the product.
+     * @param array $product The product to get the image source.
+     * @return string
+     */
     private function get_image_src(array $product)
     {
         if ($product['image1'] === "noimg.jpg") {
@@ -47,5 +45,44 @@ class Product extends Component
             $image_src = PRODUCT_IMAGE_SITE_PATH . "{$product['root_name']}/{$product['image1']}";
         }
         return $image_src;
+    }
+    /**
+     * Renders 'Free Shipment' element if the product has free shipment.
+     */
+    private function render_shipment_element(array $product)
+    {
+        if ($product['shipment'] === '1') {
+            $body = <<<HTML
+                <span class="free-shipment">
+                    <p>Ücretsiz Kargo</p>
+                </span>
+            HTML;
+        } else {
+            $body = '';
+        }
+
+        return $body;
+    }
+    /**
+     * Renders 'Add to wishlist' element and 'Remove from wishlist' element based on if the user has added the product to wishlist.
+     */
+    private function render_wishlist_element(bool $condition)
+    {
+        // TODO Get if user has added the product to wishlist and set $condition based on that.
+        if (!$condition) {
+            $body = <<<HTML
+            <span class="add-wishlist" title="Favorilere Ekle">
+                <i class="fa-solid fa-heart"></i>
+            </span>
+        HTML;
+        } else {
+            $body = <<<HTML
+            <span class="add-wishlist" title="Favorilerden Çıkar">
+                <i class="fa-solid fa-heart-broken"></i>
+            </span>
+        HTML;
+        }
+
+        return $body;
     }
 }
