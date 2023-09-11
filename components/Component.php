5e30bb82da206c07cf5448f6f482c779da202974
sscorpi <therenaydin@gmail.com>
2023-09-11 19:08:09 +0300

    Updated shorten_string function

--- components/Component.php
+++ components/Component.php
@@ -15,10 +15,21 @@ class Component
         echo $body;
     }
 
+    /**
+     * Shortens the string to the specified length.
+     * @param string $str The string to shorten.
+     * @param int $length The length to shorten the string.
+     * @return string
+     */
     public function shorten_string(string $str, int $length)
     {
         if (strlen($str) > $length) {
-            $str = substr($str, 0, $length) . '...';
+            $str = substr($str, 0, $length);
+            $last_char = substr($str, -1);
+            if ($last_char === ' ') {
+                $str = substr($str, 0, -1);
+            }
+            $str .= '...';
         }
         return $str;
     }
