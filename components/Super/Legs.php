b305a09d78f400ede673d81fa48a223ac2d542ff
sscorpi <therenaydin@gmail.com>
2023-09-14 18:09:24 +0300

    Updated js source

--- components/Super/Legs.php
+++ components/Super/Legs.php
@@ -12,7 +12,7 @@ class Legs extends Component
     public function __construct()
     {
         $body = <<<HTML
-            <script src="/dist/core/dr50hzx.js"></script>
+            <script src="/dist/core/r9k2p4i7h0o1g5w6a2u3.js"></script>
             </body>
             <!--       
 
