410c1bbb9efeaef390e42607299d989769438c2d
sscorpi <therenaydin@gmail.com>
2023-09-14 18:08:37 +0300

    Updated entry points

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,21 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: {
-    "auth/a578a8g": path.resolve(__dirname, "src/auth/index.ts"),
-    "cart/5a8a8g": path.resolve(__dirname, "src/cart/index.ts"),
-    "products/g5120x": path.resolve(__dirname, "src/products/index.ts"),
-    "dashboard/du48gn1": path.resolve(__dirname, "src/dashboard/index.ts"),
-    "profile/eb23ax4": path.resolve(__dirname, "src/profile/index.ts"),
-    "core/dr50hzx": path.resolve(__dirname, "src/core.ts"),
+    "auth/r9k2p4i7h0o1g5w6a2u3": path.resolve(__dirname, "src/auth/index.ts"),
+    "cart/v8x3q6t9j2s5f0e1n7z4": path.resolve(__dirname, "src/cart/index.ts"),
+    "products/m1l5d9y6b3r2n7o8c0s": path.resolve(
+      __dirname,
+      "src/products/index.ts"
+    ),
+    "dashboard/p2w4z9o5y8v3q6i1r7": path.resolve(
+      __dirname,
+      "src/dashboard/index.ts"
+    ),
+    "profile/k7u4h0g3t5s9e1c6q2": path.resolve(
+      __dirname,
+      "src/profile/index.ts"
+    ),
+    "core/r9k2p4i7h0o1g5w6a2u3": path.resolve(__dirname, "src/core.ts"),
   },
   output: {
     path: path.resolve(__dirname, "dist"),
