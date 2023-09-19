4895c626cbc13e23b3e28dda08fc5c1b76256eb5
sscorpi <therenaydin@gmail.com>
2023-09-19 21:12:43 +0300

    Added categories component and set categories

--- pages/products.php
+++ pages/products.php
@@ -6,25 +6,43 @@ require_once("{$_SERVER['DOCUMENT_ROOT']}/vendor/autoload.php");
 use Components\Super\Head;
 use Components\Navbar\Navbar;
 use Components\Footer\Footer;
+use Components\Categories\Categories;
 use Components\Product\Filters;
 use Components\Banners\TopBanner;
 use Components\Super\Legs;
 
+// Set stylesheets
 $styles = [
     "/dist/products/m1l5d9y6b3r2n7o8c0s.css"
 ];
 
+// Get category from URL
+$category = $_GET['category'] ?? null;
+$category = htmlspecialchars($category);
+
+$categories = [
+    'stereo' => 'Müzik Setleri',
+    'speakers' => 'Hoparlörler',
+    'turntables' => 'Plak Çalarlar',
+    'music-players' => 'Müzik Çalarlar',
+    'tapes-records' => 'Kasetler & Plaklar'
+];
+
+$title = $categories[$category] ?? 'Ürünler';
+$title = "{$title} - Bizim Shop";
+
 $head = new Head([
-    "title" => "Ürünler - Bizim Shop",
+    "title" => $title,
     "styles" => $styles,
 ]);
 $top_banner = new TopBanner();
 $navbar = new Navbar();
+$categories = new Categories();
 
 ?>
 <section id="featured" class="page-content">
     <?php
-    $filters = new Filters();
+    $filters = new Filters($category);
     ?>
     <div class="product-container">
     </div>
