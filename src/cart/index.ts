60cb601bb68a609df0b5dcad81747cab3a0dac2e
sscorpi <therenaydin@gmail.com>
2023-09-20 23:46:58 +0300

    Moved files

--- src/cart/index.ts
+++
@@ -1,9 +0,0 @@
-import initShoppingCart from "./cartManager";
-import initLikedProducts from "./initLiked";
-import initLatestProducts from "./initLastViewed";
-
-import "./cart.css";
-
-initShoppingCart();
-initLatestProducts();
-initLikedProducts();
