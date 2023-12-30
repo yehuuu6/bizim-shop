bfc6cb14d441d4a571e22d53bf89df15ad75fec0
sscorpi <therenaydin@gmail.com>
2023-12-30 16:44:45 +0300

    Optimized

--- api/authorization/forgot-password.php
+++ api/authorization/forgot-password.php
@@ -6,12 +6,17 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH
 
     $email = get_safe_value($con, $_POST['email']);
 
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        send_error_response('Lütfen geçerli bir e-posta adresi giriniz.', 'email');
+    $errors = [
+        'E-posta alanı boş bırakılamaz.' => empty($email),
+        'Lütfen geçerli bir e-posta adresi giriniz.' => !filter_var($email, FILTER_VALIDATE_EMAIL),
+    ];
+
+    foreach ($errors as $message => $condition) {
+        if ($condition) {
+            send_error_response($message, 'email');
+        }
     }
-    if (empty($email)) {
-        send_error_response('E-posta alanı boş bırakılamaz.', 'email');
-    }
+
     $email_query = "SELECT token FROM users WHERE email=? LIMIT 1";
     $stmt = $con->prepare($email_query);
     $stmt->bind_param('s', $email);
@@ -20,7 +25,13 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH
     $userCount = $result->num_rows;
     $stmt->close();
     @$token = $result->fetch_assoc()['token'];
-    $userCount > 0 ? send_password_reset_link($email, $token) : sleep(4);
+
+    if ($userCount > 0) {
+        send_password_reset_link($email, $token);
+    } else {
+        sleep(4);
+    }
+
     send_success_response('Eğer bu e-posta adresi sistemimizde kayıtlı ise, şifre sıfırlama linki gönderilmiştir.');
 } else {
     header("HTTP/1.1 403 Forbidden");
