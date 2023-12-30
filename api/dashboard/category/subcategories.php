84dfc6f1f47efbba357399c9ef83793e6cd87ae1
sscorpi <therenaydin@gmail.com>
2023-12-30 12:59:42 +0300

    Skipped uncategorized products

--- api/dashboard/category/subcategories.php
+++ api/dashboard/category/subcategories.php
@@ -7,8 +7,6 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH
 
     $result = array();
 
-    // Get post data
-
     $sql = "SELECT id, cid, name FROM subcats";
     $res = mysqli_query($con, $sql);
     $category_count = mysqli_num_rows($res);
@@ -16,6 +14,10 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH
     // Send sub category list to client
     if ($category_count > 0) {
         while ($row = mysqli_fetch_assoc($res)) {
+            // Skip Uncategorized category
+            if ($row['id'] == 0) {
+                continue;
+            }
             $result[] = $row;
         }
     }
