9bc986aaeef01f767f35fd99c74310bf68e1fcd8
sscorpi <therenaydin@gmail.com>
2023-12-30 12:59:31 +0300

    Updated products after sub category deletion

--- api/dashboard/category/delete-subcategory.php
+++ api/dashboard/category/delete-subcategory.php
@@ -18,7 +18,17 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH
         mysqli_stmt_bind_param($stmt, 'i', $id);
         try {
             mysqli_stmt_execute($stmt);
-            send_success_response("Alt kategori başarıyla silindi.");
+            $sql2 = "UPDATE product SET subcategory = 0 WHERE subcategory = ?";
+            $stmt2 = mysqli_prepare($con, $sql2);
+            mysqli_stmt_bind_param($stmt2, 'i', $id);
+            try {
+                mysqli_stmt_execute($stmt2);
+                send_success_response("Alt kategori başarıyla silindi.");
+            } catch (Exception $e) {
+                send_error_response("{$e->getMessage()}");
+            } finally {
+                mysqli_stmt_close($stmt2);
+            }
         } catch (Exception $e) {
             send_error_response("{$e->getMessage()}");
         } finally {
