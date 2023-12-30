750093af3c03d4a1dfc1e40d02f358b6dea6a783
sscorpi <therenaydin@gmail.com>
2023-12-30 13:24:54 +0300

    Implemented dynamic categories

--- pages/products.php
+++ pages/products.php
@@ -2,6 +2,7 @@
 define('FILE_ACCESS', TRUE);
 
 require_once("{$_SERVER['DOCUMENT_ROOT']}/vendor/autoload.php");
+require_once("{$_SERVER['DOCUMENT_ROOT']}/includes/auth.inc.php");
 
 use Components\Super\Head;
 use Components\Navbar\Navbar;
@@ -15,20 +16,50 @@ use Components\Super\Legs;
 $styles = [
     "/dist/products/m1l5d9y6b3r2n7o8c0s.css"
 ];
+global $con;
+
+// If slug is not set, go back.
+if (!isset($_GET['category'])) {
+    header("Location: /");
+    exit;
+}
 
 // Get category from URL
-$category = $_GET['category'] ?? null;
-$category = htmlspecialchars($category);
+$slug = isset($_GET['category']) ? get_safe_value($con, $_GET['category']) : "0";
 
-$categories = [
-    'stereo' => 'Müzik Setleri',
-    'speakers' => 'Hoparlörler',
-    'turntables' => 'Plak Çalarlar',
-    'music-players' => 'Müzik Çalarlar',
-    'tapes-records' => 'Kasetler & Plaklar'
-];
+// Get sub category from URL
+$sub_category_slug = isset($_GET['sub-category']) ? get_safe_value($con, $_GET['sub-category']) : "Tümünü Göster";
 
-$title = $categories[$category] ?? 'Ürünler';
+$page_num = $_GET['page'] ?? 1;
+
+$total_product_count = 0;
+
+$sql = "SELECT id,name,slug FROM categories WHERE slug = ?";
+$stmt = mysqli_prepare($con, $sql);
+mysqli_stmt_bind_param($stmt, 's', $slug);
+mysqli_stmt_execute($stmt);
+$res = mysqli_stmt_get_result($stmt);
+$category_data = mysqli_fetch_row($res) ?? null;
+
+# 0 => id
+# 1 => name
+# 2 => slug
+# for the category
+
+if ($category_data === null) {
+    $sql = "SELECT COUNT(*) FROM product";
+    $stmt = mysqli_prepare($con, $sql);
+} else {
+    $sql = "SELECT COUNT(*) FROM product WHERE category = ?";
+    $stmt = mysqli_prepare($con, $sql);
+    $stmt->bind_param('i', $category_data[0]);
+}
+mysqli_stmt_execute($stmt);
+$res = mysqli_stmt_get_result($stmt);
+
+$total_product_count = mysqli_fetch_row($res)[0];
+
+$title = $category_data[1] ?? 'Tüm Ürünler';
 $title = "{$title} - Bizim Shop";
 
 $head = new Head([
@@ -37,14 +68,37 @@ $head = new Head([
 ]);
 $top_banner = new TopBanner();
 $navbar = new Navbar();
-$categories = new Categories();
 
 ?>
+
+<div class="categories-container">
+    <ul class="categories">
+        <?php $categories = new Categories(); ?>
+    </ul>
+</div>
+
+<div class="router">
+    <a href="/">Ana Sayfa</a>
+    <?php if ($category_data !== null) : ?>
+        <a href="/products/<?= $category_data[2] ?>">> <?= $category_data[1] ?></a>
+    <?php endif; ?>
+</div>
 <section id="product-lister" class="page-content">
     <?php
-    $filters = new Filters($category);
+    $filters = new Filters($slug, $category_data[0], $sub_category_slug);
+    $encoded_sub_category_slug = urlencode(urlencode($sub_category_slug));
     ?>
     <div class="product-container">
+        <div class="products">
+        </div>
+        <div class="page-numbers">
+            <!---- Page buttons does not update when filters applied, FIX IT -->
+            <?php foreach (range(1, ceil($total_product_count / 50)) as $page) : ?>
+                <?php if ($page != 0) : ?>
+                    <a href="/products<?= $slug !== "" ? "/{$slug}" : '' ?><?= $sub_category_slug !== "" ? "/{$encoded_sub_category_slug}" : '' ?>?page=<?= $page ?>" class="page-number <?= $page == $page_num ? 'active' : '' ?>"><?= $page ?></a>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </div>
     </div>
 </section>
 <script src="/dist/products/m1l5d9y6b3r2n7o8c0s.js"></script>
