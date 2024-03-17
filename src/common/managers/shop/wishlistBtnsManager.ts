2899e83752edd989a7ef8a172c2f7663803e6002
yehuuu6 <therenaydin@gmail.com>
2024-03-17 12:59:28 +0300

    Made liked products database controlled instead of local storage

--- src/common/managers/shop/wishlistBtnsManager.ts
+++ src/common/managers/shop/wishlistBtnsManager.ts
@@ -1,45 +1,56 @@
+import {
+  addToWishlist,
+  checkWishlistItem,
+  pullMyWishlist,
+  removeFromWishlist,
+} from '@/common/funcs/functions.likes';
+
 // Define a function to update the wishlist button for a single product
 function updateWishlistButton(product: HTMLDivElement) {
-  const btn = product.querySelector(".add-wishlist") as HTMLSpanElement;
+  const btn = product.querySelector('.add-wishlist') as HTMLSpanElement;
   const id = product.dataset.id as string;
 
   function updateBtnContent(btn: HTMLSpanElement, isInWishlist: boolean) {
     btn.innerHTML = isInWishlist
       ? '<i class="fa-solid fa-heart-broken"></i>'
       : '<i class="fa-solid fa-heart"></i>';
-    btn.title = isInWishlist ? "Favorilerden Çıkar" : "Favorilere Ekle";
+    btn.title = isInWishlist ? 'Favorilerden Çıkar' : 'Favorilere Ekle';
   }
 
-  btn.addEventListener("click", () => {
-    let wishlistItems: string[] = JSON.parse(
-      localStorage.getItem("wishlist") || "[]"
-    );
-    const isInWishlist = wishlistItems.includes(id);
-    if (isInWishlist) {
-      // Remove the item from the wishlist
-      wishlistItems = wishlistItems.filter((item) => item !== id);
-
-      // Update the button and store the wishlist items in local storage
-      localStorage.setItem("wishlist", JSON.stringify(wishlistItems));
-      setNavbarWishItemCount();
-      updateBtnContent(btn, false);
-    } else {
-      // Add the item to the wishlist
-      wishlistItems.push(id);
-
-      // Update the button and store the wishlist items in local storage
-      localStorage.setItem("wishlist", JSON.stringify(wishlistItems));
-      setNavbarWishItemCount();
-      updateBtnContent(btn, true);
-    }
+  btn.addEventListener('click', () => {
+    checkWishlistItem(id).then((response) => {
+      const [result, message, cause] = response.data;
+      if (result == 'success' && cause == 'in_wishlist') {
+        // Means the product is in the wishlist, so remove it
+        removeFromWishlist(id).then((response) => {
+          const [result, message, cause] = response.data;
+          if (result === 'success') {
+            updateBtnContent(btn, false);
+            setNavbarWishItemCount();
+          }
+        });
+      } else if (result === 'error' && cause === 'login') {
+        window.location.href = '/auth/login';
+      } else if (result === 'error' && cause === 'not_in_wishlist') {
+        // Means the product is not in the wishlist, so add it
+        addToWishlist(id).then((response) => {
+          const [result, message, cause] = response.data;
+          if (result === 'success') {
+            updateBtnContent(btn, true);
+            setNavbarWishItemCount();
+          }
+        });
+      }
+    });
   });
 
   // Initialize the button state
-  const wishlistItems: string[] = JSON.parse(
-    localStorage.getItem("wishlist") || "[]"
-  );
-  const isInWishlist = wishlistItems.includes(id);
-  updateBtnContent(btn, isInWishlist);
+  // Get wishlist items from pullMyWishlist not from localStorage
+  pullMyWishlist().then((response) => {
+    const wishlistItems = response.data;
+    const isInWishlist = wishlistItems.includes(Number(id));
+    updateBtnContent(btn, isInWishlist);
+  });
 }
 
 export function setWishlistBtns(products: NodeListOf<HTMLDivElement>) {
@@ -49,9 +60,15 @@ export function setWishlistBtns(products: NodeListOf<HTMLDivElement>) {
 }
 
 export function setNavbarWishItemCount() {
-  const items = JSON.parse(localStorage.getItem("wishlist") || "[]");
-  const count = document.querySelector(
-    "#navbar-wishlist-count"
-  ) as HTMLSpanElement;
-  count.innerText = `${items.length}`;
+  pullMyWishlist().then((response) => {
+    const [result, message, cause] = response.data;
+    if (result === 'error' && cause === 'login') {
+      return;
+    }
+    const wishlistItems = response.data;
+    const count = document.querySelector(
+      '#navbar-wishlist-count'
+    ) as HTMLSpanElement;
+    count.innerText = `${wishlistItems.length}`;
+  });
 }
