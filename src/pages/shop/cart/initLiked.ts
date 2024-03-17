eb3341b370d3b795179f9b5b7604a16bae22371b
yehuuu6 <therenaydin@gmail.com>
2024-03-17 13:01:51 +0300

    Update liked product manager to database instead of local storage

--- src/pages/shop/cart/initLiked.ts
+++ src/pages/shop/cart/initLiked.ts
@@ -1,82 +1,94 @@
-import { getProductsById } from "@/pages/shop/utility/getProducts";
-import { setWishlistBtns } from "@/common/managers/shop/wishlistBtnsManager";
-import { setAddToCartBtns } from "@/common/managers/shop/cartBtnsManager";
-import initShoppingCart from "./cartManager";
-import { resetShowcases } from ".";
+import { getProductsById } from '@/pages/shop/utility/getProducts';
+import { setWishlistBtns } from '@/common/managers/shop/wishlistBtnsManager';
+import { setAddToCartBtns } from '@/common/managers/shop/cartBtnsManager';
+import initShoppingCart from './cartManager';
+import { resetShowcases } from '.';
+import { pullMyWishlist } from '@/common/funcs/functions.likes';
 
-import "@/pages/shop/products/products.css";
+import '@/pages/shop/products/products.css';
 
 const likedProductsContainer = document.querySelector(
-  "#liked-products"
+  '#liked-products'
 ) as HTMLDivElement;
 
 const likedItemCounter = document.querySelector(
-  "#liked-item-counter"
+  '#liked-item-counter'
 ) as HTMLSpanElement;
 
 /**
- * Initializes liked products by getting products from localStorage.
+ * Initializes liked products by pulling the wishlist from db and rendering the products.
  */
 export default function initLikedProducts() {
-  likedProductsContainer.innerHTML = "";
-  const likedProducts = JSON.parse(localStorage.getItem("wishlist") || "[]");
-  getProductsById(createFormData(likedProducts))
-    .then((products) => {
-      if (products.length < 1) {
-        likedProductsContainer.innerHTML +=
-          '<div class="no-products"><h2><i class="fa-solid fa-heart-broken"></i> Beğendiğiniz ürün bulunmamaktadır.</h2></div>';
-      } else {
-        products.forEach((product: string) => {
-          likedProductsContainer.innerHTML += product;
+  likedProductsContainer.innerHTML = '';
+  pullMyWishlist().then((response) => {
+    if (
+      response.data.length > 1 &&
+      response.data[0] === 'error' &&
+      response.data[2] === 'login'
+    ) {
+      likedProductsContainer.innerHTML +=
+        '<div class="no-products"><h2><i class="fa-solid fa-heart"></i> Beğendiğiniz ürünleri görebilmek için <a class="link blue-text" href="/auth/login">giriş</a> yapmalısınız.</h2></div>';
+      return;
+    }
+    const likedProducts = response.data;
+    getProductsById(createFormData(likedProducts))
+      .then((products) => {
+        if (products.length < 1) {
+          likedProductsContainer.innerHTML +=
+            '<div class="no-products"><h2><i class="fa-solid fa-heart-broken"></i> Beğendiğiniz ürün bulunmamaktadır.</h2></div>';
+        } else {
+          products.forEach((product: string) => {
+            likedProductsContainer.innerHTML += product;
+          });
+        }
+      })
+      .finally(() => {
+        const productElements = likedProductsContainer.querySelectorAll(
+          '.product'
+        ) as NodeListOf<HTMLDivElement>;
+
+        // Set liked item counter
+        const productCount = productElements.length;
+        likedItemCounter.innerText = `(${likedProducts.length} ürün, ${productCount} tane gösteriliyor)`;
+
+        // Set wishlist buttons
+        setWishlistBtns(productElements);
+        // Set add to cart buttons
+        setAddToCartBtns(productElements);
+
+        const cartBtns = likedProductsContainer.querySelectorAll(
+          '.product #product-cart-btn'
+        ) as NodeListOf<HTMLButtonElement>;
+
+        cartBtns.forEach((btn) => {
+          btn.addEventListener('click', () => {
+            // Refresh the shopping cart and showcases
+            initShoppingCart();
+            const id = (btn.closest('.product') as HTMLDivElement).dataset
+              .id as string;
+            resetShowcases(id, 'cart');
+          });
         });
-      }
-    })
-    .finally(() => {
-      const productElements = likedProductsContainer.querySelectorAll(
-        ".product"
-      ) as NodeListOf<HTMLDivElement>;
 
-      // Set liked item counter
-      const productCount = productElements.length;
-      likedItemCounter.innerText = `(${likedProducts.length} ürün, ${productCount} tane gösteriliyor)`;
+        const wishlistBtns = likedProductsContainer.querySelectorAll(
+          "span[class^='add-wishlist']"
+        ) as NodeListOf<HTMLSpanElement>;
 
-      // Set wishlist buttons
-      setWishlistBtns(productElements);
-      // Set add to cart buttons
-      setAddToCartBtns(productElements);
-
-      const cartBtns = likedProductsContainer.querySelectorAll(
-        ".product #product-cart-btn"
-      ) as NodeListOf<HTMLButtonElement>;
-
-      cartBtns.forEach((btn) => {
-        btn.addEventListener("click", () => {
-          // Refresh the shopping cart and showcases
-          initShoppingCart();
-          const id = (btn.closest(".product") as HTMLDivElement).dataset
-            .id as string;
-          resetShowcases(id, "cart");
+        wishlistBtns.forEach((btn) => {
+          btn.addEventListener('click', () => {
+            const id = (btn.closest('.product') as HTMLDivElement).dataset
+              .id as string;
+            resetShowcases(id, 'wishlist');
+          });
         });
       });
-
-      const wishlistBtns = likedProductsContainer.querySelectorAll(
-        "span[class^='add-wishlist']"
-      ) as NodeListOf<HTMLSpanElement>;
-
-      wishlistBtns.forEach((btn) => {
-        btn.addEventListener("click", () => {
-          const id = (btn.closest(".product") as HTMLDivElement).dataset
-            .id as string;
-          resetShowcases(id, "wishlist");
-        });
-      });
-    });
+  });
 }
 
 function createFormData(likedProducts: string) {
   const formData = new FormData();
-  formData.append("product-ids", likedProducts);
-  formData.append("product-type", "default");
-  formData.append("limit", "5");
+  formData.append('product-ids', likedProducts);
+  formData.append('product-type', 'default');
+  formData.append('limit', '5');
   return formData;
 }
--- src/pages/shop/wishlist/index.ts
+++ src/pages/shop/wishlist/index.ts
@@ -1,43 +1,55 @@
-import { getProductsById } from "@/pages/shop/utility/getProducts";
-import { setWishlistBtns } from "@/common/managers/shop/wishlistBtnsManager";
-import { setAddToCartBtns } from "@/common/managers/shop/cartBtnsManager";
+import { getProductsById } from '@/pages/shop/utility/getProducts';
+import { setWishlistBtns } from '@/common/managers/shop/wishlistBtnsManager';
+import { setAddToCartBtns } from '@/common/managers/shop/cartBtnsManager';
+import { pullMyWishlist } from '@/common/funcs/functions.likes';
 
-// Get wishlist products from local storage
-const wishlistProducts = JSON.parse(localStorage.getItem("wishlist") || "[]");
-
-const wishlistContainer = document.querySelector(".products") as HTMLDivElement;
+const wishlistContainer = document.querySelector('.products') as HTMLDivElement;
 
 function initWishlistedProducts() {
-  wishlistContainer.classList.add("dynamic-content");
-  wishlistContainer.innerHTML = "";
+  wishlistContainer.classList.add('dynamic-content');
+  wishlistContainer.innerHTML = '';
   const formData = new FormData();
-  formData.append("product-ids", wishlistProducts);
-  formData.append("product-type", "default");
-  getProductsById(formData)
-    .then((products) => {
-      if (products.length < 1) {
-        wishlistContainer.style.display = "flex";
-        wishlistContainer.style.justifyContent = "center";
-        wishlistContainer.style.alignItems = "center";
-        wishlistContainer.innerHTML +=
-          '<div class="no-products"><h2><i class="fas fa-heart"></i> Beğendiğiniz ürün bulunmamaktadır.</h2></div>';
-      } else {
-        products.forEach((product: string) => {
-          wishlistContainer.innerHTML += product;
-        });
-      }
-    })
-    .finally(() => {
-      const products = wishlistContainer.querySelectorAll(
-        ".product"
-      ) as NodeListOf<HTMLDivElement>;
+  pullMyWishlist().then((response) => {
+    if (
+      response.data.length > 1 &&
+      response.data[0] === 'error' &&
+      response.data[2] === 'login'
+    ) {
+      wishlistContainer.style.display = 'flex';
+      wishlistContainer.style.justifyContent = 'center';
+      wishlistContainer.style.alignItems = 'center';
+      wishlistContainer.innerHTML +=
+        '<div class="no-products"><h2><i class="fas fa-heart"></i> Beğendiğiniz ürünleri görebilmek için <a class="link blue-text" href="/auth/login">giriş</a> yapmalısınız.</h2></div>';
+      return;
+    }
+    formData.append('product-ids', response.data);
+    formData.append('product-type', 'default');
+    getProductsById(formData)
+      .then((products) => {
+        if (products.length < 1) {
+          wishlistContainer.style.display = 'flex';
+          wishlistContainer.style.justifyContent = 'center';
+          wishlistContainer.style.alignItems = 'center';
+          wishlistContainer.innerHTML +=
+            '<div class="no-products"><h2><i class="fas fa-heart"></i> Beğendiğiniz ürün bulunmamaktadır.</h2></div>';
+        } else {
+          products.forEach((product: string) => {
+            wishlistContainer.innerHTML += product;
+          });
+        }
+      })
+      .finally(() => {
+        const products = wishlistContainer.querySelectorAll(
+          '.product'
+        ) as NodeListOf<HTMLDivElement>;
 
-      setWishlistBtns(products);
-      setAddToCartBtns(products);
-      setTimeout(() => {
-        wishlistContainer.classList.remove("dynamic-content");
-      }, 850);
-    });
+        setWishlistBtns(products);
+        setAddToCartBtns(products);
+        setTimeout(() => {
+          wishlistContainer.classList.remove('dynamic-content');
+        }, 850);
+      });
+  });
 }
 
 initWishlistedProducts();
