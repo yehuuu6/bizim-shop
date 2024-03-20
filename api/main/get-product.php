af20839a0f2b9847467f99e048edb1054126d3f6
yehuuu6 <therenaydin@gmail.com>
2024-03-20 22:24:14 +0300

    Optimize code

--- api/main/get-product.php
+++ api/main/get-product.php
@@ -5,21 +5,19 @@ require_once("{$_SERVER['DOCUMENT_ROOT']}/vendor/autoload.php");
 
 use Components\Product\Cards\AddedToCart;
 
-if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')) {
-    require_once("{$_SERVER['DOCUMENT_ROOT']}/includes/auth.inc.php");
+require_once("{$_SERVER['DOCUMENT_ROOT']}/includes/auth.inc.php");
 
-    $product_id = get_safe_value($con, $_POST['id']);
-
-    $product_data = get_products($con, [
-        'id' => $product_id,
-        'limit' => '1',
-    ]);
-
-    $product = new AddedToCart($product_data[0]);
-
-    echo json_encode($product->body);
-} else {
-    header("HTTP/1.1 403 Forbidden");
-    include($_SERVER['DOCUMENT_ROOT'] . '/errors/403.php');
-    exit;
+if (!validate_request()) {
+    send_forbidden_response();
 }
+
+$product_id = get_safe_value($con, $_POST['id']);
+
+$product_data = get_products($con, [
+    'id' => $product_id,
+    'limit' => '1',
+]);
+
+$product = new AddedToCart($product_data[0]);
+
+echo json_encode($product->body);
