a33081e7b5f72db5c26fbb5d8300364a7cd76e72
yehuuu6 <therenaydin@gmail.com>
2024-03-23 20:23:29 +0300

    Add offset, limit and search params to query

--- api/dashboard/orders/load-orders.php
+++ api/dashboard/orders/load-orders.php
@@ -13,25 +13,24 @@ $result = array();
 
 // Get post data
 
-$start = get_safe_value($con, $_POST['start']);
+$offset = get_safe_value($con, $_POST['offset']);
+$limit = get_safe_value($con, $_POST['limit']);
 
-$sql = "SELECT * FROM ORDERS LIMIT $start, 10";
-$res = mysqli_query($con, $sql);
-$order_count = mysqli_num_rows($res);
+if (isset($_POST['search'])) {
+    $search = get_safe_value($con, $_POST['search']);
+} else {
+    $search = '';
+}
+
+$stmt = $con->prepare("SELECT * from orders LIMIT ? OFFSET ?");
+$stmt->bind_param("ii", $limit, $offset);
+$stmt->execute();
+$res = $stmt->get_result();
+$order_count = $res->num_rows;
 
 // Send order list to client
 if ($order_count > 0) {
-    while ($row = mysqli_fetch_assoc($res)) {
-        $sql1 = "SELECT name, surname FROM users WHERE id = '{$row['uid']}'";
-        $res1 = mysqli_query($con, $sql1);
-        $user = mysqli_fetch_assoc($res1);
-        $row['user_name'] = $user['name'] . ' ' . $user['surname'];
-
-        $sql2 = "SELECT name, price FROM product WHERE id = '{$row['pid']}'";
-        $res2 = mysqli_query($con, $sql2);
-        $product = mysqli_fetch_assoc($res2);
-        $row['product_name'] = $product['name'];
-        $row['product_price'] = $product['price'];
+    while ($row = $res->fetch_assoc()) {
         $result[] = $row;
     }
 }
