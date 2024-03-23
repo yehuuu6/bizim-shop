377fee88cfe80e6aba00c52e1756d1727e18baf1
yehuuu6 <therenaydin@gmail.com>
2024-03-23 20:23:40 +0300

    Add search params to query

--- api/dashboard/product/load-products.php
+++ api/dashboard/product/load-products.php
@@ -14,9 +14,16 @@ authorize_user();
 $offset = get_safe_value($con, $_POST['offset']);
 $limit = get_safe_value($con, $_POST['limit']);
 
+if (isset($_POST['search'])) {
+    $search = get_safe_value($con, $_POST['search']);
+} else {
+    $search = '';
+}
+
 $products = get_products($con, [
     'order_type' => 'id ASC',
     'limit' => $limit,
+    'search' => $search,
     'offset' => $offset,
     'status' => ''
 ]);
