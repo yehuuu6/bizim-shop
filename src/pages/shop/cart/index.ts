87e72726e3d426420fcd4609af0b70504ae7dd35
yehuuu6 <therenaydin@gmail.com>
2024-03-23 13:45:13 +0300

    Remove old update wishlist buttons & add sticky style to checkout

--- src/pages/shop/cart/index.ts
+++ src/pages/shop/cart/index.ts
@@ -1,12 +1,28 @@
-import initShoppingCart from "./cartManager";
-import initLikedProducts from "./initLiked";
-import initLatestProducts from "./initLastViewed";
+import initShoppingCart from './cartManager';
+import initLikedProducts from './initLiked';
+import initLatestProducts from './initLastViewed';
 
-import "./cart.css";
+import './cart.css';
 
 initShoppingCart();
 initLatestProducts();
 initLikedProducts();
+
+window.addEventListener('scroll', () => {
+  const cartWrapper = document.querySelector(
+    '.shopping-cart'
+  ) as HTMLDivElement;
+  const stickyPoint = cartWrapper.offsetTop;
+  const stickyItem = cartWrapper.querySelector('.checkout') as HTMLDivElement;
+  const bottomLimit = cartWrapper.offsetTop + cartWrapper.offsetHeight;
+
+  if (
+    window.scrollY >= stickyPoint &&
+    window.scrollY + stickyItem.offsetHeight < bottomLimit
+  ) {
+    stickyItem.classList.add('sticky');
+  }
+});
 
 /**
  * Updates product buttons in showcases.
@@ -14,10 +30,9 @@ initLikedProducts();
  * @param type Type of the showcase to be updated (cart, wishlist)
  */
 export function resetShowcases(productId: string, type: string) {
-  const inCart = localStorage.getItem("cart") || "[]";
-  const inWishlist = localStorage.getItem("wishlist") || "[]";
+  const inCart = JSON.parse(localStorage.getItem('cart') || '[]');
   const showcases = document.querySelectorAll(
-    ".product-showcase"
+    '.product-showcase'
   ) as NodeListOf<HTMLDivElement>;
   showcases.forEach((showcase) => {
     const productElement = showcase.querySelector(
@@ -25,34 +40,20 @@ export function resetShowcases(productId: string, type: string) {
     ) as HTMLDivElement;
     function updateCartBtns() {
       const btn = productElement.querySelector(
-        "#product-cart-btn"
+        '#product-cart-btn'
       ) as HTMLButtonElement;
       if (inCart.includes(productId)) {
-        btn.className = "in-cart";
-        btn.innerText = "Sepette";
+        btn.className = 'in-cart';
+        btn.innerText = 'Sepette';
         btn.disabled = true;
       } else {
-        btn.className = "add-cart";
-        btn.innerText = "Sepete Ekle";
+        btn.className = 'add-cart';
+        btn.innerText = 'Sepete Ekle';
         btn.disabled = false;
       }
     }
-    function updateWishlistBtns() {
-      const btn = productElement.querySelector(
-        ".add-wishlist"
-      ) as HTMLButtonElement;
-      if (inWishlist.includes(productId)) {
-        btn.innerText = "Favorilerden Çıkar";
-        btn.innerHTML = '<i class="fa-solid fa-heart-broken"></i>';
-      } else {
-        btn.innerText = "Favorilere Ekle";
-        btn.innerHTML = '<i class="fa-solid fa-heart"></i>';
-      }
-    }
-    if (productElement && type === "cart") {
+    if (productElement && type === 'cart') {
       updateCartBtns();
-    } else if (productElement && type === "wishlist") {
-      updateWishlistBtns();
     }
   });
 }
