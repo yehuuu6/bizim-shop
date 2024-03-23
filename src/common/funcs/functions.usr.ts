c55bbeaf8c8f10437491b7535143e685aa6dab82
yehuuu6 <therenaydin@gmail.com>
2024-03-24 01:18:46 +0300

    Add set order status function

--- src/common/funcs/functions.usr.ts
+++ src/common/funcs/functions.usr.ts
@@ -1,4 +1,4 @@
-import PanelClass from "@/classes/PanelController";
+import PanelClass from '@/classes/PanelController';
 
 /**
  * Directly gets response and displays the message. Use this if you don't need to do stuff with response.
@@ -18,38 +18,74 @@ export function getApiResponse(
   });
 }
 
+// 0 = Beklemede, 1 = Hazırlanıyor, 2 = Kargoya Verildi, 3 = Teslim Edildi, 4 = İptal Edildi, 5 = İade Edildi, 6 = Tamamlandı, 7 = Tamamlanmadı
+export function setOrderStatus(status: string) {
+  let statusText: string | undefined = 'Hata';
+
+  switch (status) {
+    case '0':
+      statusText = 'Beklemede';
+      break;
+    case '1':
+      statusText = 'Hazırlanıyor';
+      break;
+    case '2':
+      statusText = 'Kargoya Verildi';
+      break;
+    case '3':
+      statusText = 'Teslim Edildi';
+      break;
+    case '4':
+      statusText = 'İptal Edildi';
+      break;
+    case '5':
+      statusText = 'İade Edildi';
+      break;
+    case '6':
+      statusText = 'Tamamlandı';
+      break;
+    case '7':
+      statusText = 'Tamamlanmadı';
+      break;
+    default:
+      statusText = 'Hata';
+      break;
+  }
+  return statusText;
+}
+
 export function clearAvatarInput() {
   const avatarInput: HTMLInputElement =
-    document.querySelector("#avatar-input")!;
-  avatarInput.value = "";
+    document.querySelector('#avatar-input')!;
+  avatarInput.value = '';
   const avatarInputDisplayer: HTMLParagraphElement = document.querySelector(
-    "#avatar-input-displayer"
+    '#avatar-input-displayer'
   )!;
-  avatarInputDisplayer.innerText = "Dosya seçilmedi.";
+  avatarInputDisplayer.innerText = 'Dosya seçilmedi.';
 }
 
 export function trimSentence(sentence: string, maxLength: number) {
   if (sentence.length > maxLength) {
-    sentence = sentence.substring(0, maxLength - 3) + "...";
+    sentence = sentence.substring(0, maxLength - 3) + '...';
   }
   return sentence;
 }
 
 export function getDate(raw: string) {
-  const [year, month_t, day] = raw.split("-");
+  const [year, month_t, day] = raw.split('-');
   const month_names = [
-    "Jan",
-    "Feb",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
+    'Jan',
+    'Feb',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
   ];
   const month = month_names[Number(month_t) - 1];
   const day_trimmed = Number(day).toString();
