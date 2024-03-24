e9aac9439da7816a769c266cea2922f449215de4
yehuuu6 <therenaydin@gmail.com>
2024-03-24 17:43:47 +0300

    Initial commit

---
+++ api/dashboard/questions/load-questions.php
@@ -0,0 +1,82 @@
+<?php
+define('FILE_ACCESS', TRUE);
+
+require_once("{$_SERVER['DOCUMENT_ROOT']}/includes/auth.inc.php");
+
+if (!validate_request()) {
+    send_forbidden_response();
+}
+
+authorize_user();
+
+$result = array();
+
+// Get post data
+
+$offset = get_safe_value($con, $_POST['offset']);
+$limit = get_safe_value($con, $_POST['limit']);
+
+if (isset($_POST['search'])) {
+    $search = get_safe_value($con, $_POST['search']);
+} else {
+    $search = '';
+}
+
+function get_product_data(mysqli $con, string $product_id)
+{
+    $sql = "SELECT name, root_name FROM product WHERE id = ?";
+    $stmt = $con->prepare($sql);
+    $stmt->bind_param('s', $product_id);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($name, $root_name);
+    $stmt->fetch();
+    $link = str_replace('_', '-', $root_name);
+    $link = urlencode(urlencode($link));
+    $product = [
+        'name' => $name,
+        'link' => $link
+    ];
+    $stmt->close();
+    return $product;
+}
+
+function get_user_data(mysqli $con, string $user_id)
+{
+    $sql = "SELECT name, surname FROM users WHERE id = ?";
+    $stmt = $con->prepare($sql);
+    $stmt->bind_param('s', $user_id);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($name, $surname);
+    $stmt->fetch();
+    $user = [
+        'name' => $name,
+        'surname' => $surname
+    ];
+    $stmt->close();
+    return $user;
+}
+
+try {
+    $sql = "SELECT * FROM questions LIMIT $limit OFFSET $offset";
+    $res = mysqli_query($con, $sql);
+    $orders = array();
+    while ($row = mysqli_fetch_assoc($res)) {
+        $product = get_product_data($con, $row['pid']);
+        $user = get_user_data($con, $row['uid']);
+        $order = [
+            'id' => $row['id'],
+            'username' => $user['name'] . ' ' . $user['surname'],
+            'product' => $product['name'],
+            'link' => $product['link'],
+            'date' => $row['date'],
+            'status' => $row['status']
+        ];
+        array_push($orders, $order);
+    }
+} catch (Exception $e) {
+    $orders = [$e->getMessage()];
+}
+
+echo json_encode($orders);
