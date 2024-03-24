1c76ff98288478de9d39518f2d06d7405b6760e1
yehuuu6 <therenaydin@gmail.com>
2024-03-24 11:31:53 +0300

    Update entry points

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,71 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: {
-    "control-center/managers/2j4k6s8o9t1a3v5w7y0u": path.resolve(
+    'auth/rs5f0f0e1h08v35w6a2u3': path.resolve(
       __dirname,
-      "src/common/managers/dashboard/menuManager.ts"
+      'src/pages/auth/index.ts'
     ),
-    "control-center/managers/4s9k5f7p3o8t2a0v1w6u": path.resolve(
+    'shop/cart/v8x3q6t9j2s5f0e1n7z4': path.resolve(
       __dirname,
-      "src/common/managers/dashboard/themeManager.ts"
+      'src/pages/shop/cart/index.ts'
     ),
-    "auth/rs5f0f0e1h08v35w6a2u3": path.resolve(
+    'shop/product/9k25c1l2zki6a0e1n7q6': path.resolve(
       __dirname,
-      "src/pages/auth/index.ts"
+      'src/pages/shop/product/index.ts'
     ),
-    "shop/cart/v8x3q6t9j2s5f0e1n7z4": path.resolve(
+    'shop/products/m1l5d9y6b3r2n7o8c0s': path.resolve(
       __dirname,
-      "src/pages/shop/cart/index.ts"
+      'src/pages/shop/products/index.ts'
     ),
-    "shop/product/9k25c1l2zki6a0e1n7q6": path.resolve(
+    'shop/search/s2a3g8g3n7k5x1p0vqj': path.resolve(
       __dirname,
-      "src/pages/shop/product/index.ts"
+      'src/pages/shop/search/index.ts'
     ),
-    "shop/products/m1l5d9y6b3r2n7o8c0s": path.resolve(
+    'shop/wishlist/1k2c1l5d6z3r7o8b0s4': path.resolve(
       __dirname,
-      "src/pages/shop/products/index.ts"
+      'src/pages/shop/wishlist/index.ts'
     ),
-    "shop/search/s2a3g8g3n7k5x1p0vqj": path.resolve(
+    'control-center/dashboard/p2w4z9o5y8v3q6i1r7': path.resolve(
       __dirname,
-      "src/pages/shop/search/index.ts"
+      'src/pages/control-center/dashboard/index.ts'
     ),
-    "shop/wishlist/1k2c1l5d6z3r7o8b0s4": path.resolve(
+    'control-center/account/k7u4h0g3t5s9e1c6q2': path.resolve(
       __dirname,
-      "src/pages/shop/wishlist/index.ts"
+      'src/pages/control-center/account/index.ts'
     ),
-    "control-center/dashboard/p2w4z9o5y8v3q6i1r7": path.resolve(
-      __dirname,
-      "src/pages/control-center/dashboard/index.ts"
-    ),
-    "control-center/account/k7u4h0g3t5s9e1c6q2": path.resolve(
-      __dirname,
-      "src/pages/control-center/account/index.ts"
-    ),
-    "core/r9k2p4i7h0o1g5w6a2u3": path.resolve(__dirname, "src/core/index.ts"),
+    'core/r9k2p4i7h0o1g5w6a2u3': path.resolve(__dirname, 'src/core/index.ts'),
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, { loader: "css-loader" }],
+        use: [MiniCssExtractPlugin.loader, { loader: 'css-loader' }],
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: "imgs/[name].[hash][ext]",
+          filename: 'imgs/[name].[hash][ext]',
         },
       },
     ],
@@ -104,7 +96,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[name].css",
+      filename: '[name].css',
     }),
   ],
 };
