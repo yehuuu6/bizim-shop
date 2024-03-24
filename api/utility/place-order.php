1753616895a3e089f06afa5737c62d994f283398
yehuuu6 <therenaydin@gmail.com>
2024-03-24 20:41:46 +0300

    Update validation code

--- api/utility/place-order.php
+++ api/utility/place-order.php
@@ -1,18 +1,21 @@
 <?php
 define('FILE_ACCESS', TRUE);
-if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')) {
-    require_once("{$_SERVER['DOCUMENT_ROOT']}/includes/auth.inc.php");
 
-    $user_id = get_safe_value($con, $_POST['user_id']);
-    $product_id = get_safe_value($con, $_POST['product_id']);
+require_once("{$_SERVER['DOCUMENT_ROOT']}/includes/auth.inc.php");
 
-    if (place_order($con, $user_id, $product_id)) {
-        send_success_response('Siparişiniz başarıyla alındı.');
-    } else {
-        send_error_response('Siparişiniz alınırken bir hata oluştu.');
-    }
+if (!validate_request()) {
+    send_forbidden_response();
+}
+
+if (!isset($_SESSION['id'])) {
+    send_forbidden_response();
+}
+
+$user_id = get_safe_value($con, $_POST['user_id']);
+$product_id = get_safe_value($con, $_POST['product_id']);
+
+if (place_order($con, $user_id, $product_id)) {
+    send_success_response('Siparişiniz başarıyla alındı.');
 } else {
-    header("HTTP/1.1 403 Forbidden");
-    include($_SERVER['DOCUMENT_ROOT'] . '/errors/403.php');
-    exit;
+    send_error_response('Siparişiniz alınırken bir hata oluştu.');
 }
